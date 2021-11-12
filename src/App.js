d58aa22d263aa61a08d56e2576cf2c1894ccc7bd
Mauro Brandan <maurobrandan.mb@gmail.com>
2021-11-12 12:15:49 -0300

    Componente Logo Svg

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import { GlobalStyles } from './GlobalStyles'
 
+import { Logo } from './components/Logo'
 import { ListOfCategories } from './components/ListOfCategories'
 import { ListOfPhotoCards } from './components/ListOfPhotoCards'
 
 export const App = () => (
 	<>
 		<GlobalStyles />
-		<h1>Petgram</h1>
+		<Logo />
 		<ListOfCategories />
 		<ListOfPhotoCards />
 	</>
---
+++ src/components/Logo/styles.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components'
+
+export const Svg = styled.svg`
+	width: 220px;
+	margin-left: -32px;
+	margin-top: -32px;
+`
