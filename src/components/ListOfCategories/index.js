cfa37b81ae35ed417c1f4e3df46a0f886cfad770
Mauro Brandan <maurobrandan.mb@gmail.com>
2021-11-15 12:13:18 -0300

    Fetch a Categories

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -1,16 +1,26 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { List, Item } from './styles'
 
 import { Category } from '../Category/'
 
-import { categories } from '../../../api/db.json'
+export const ListOfCategories = () => {
+	const [categories, setCategories] = useState([])
 
-export const ListOfCategories = () => (
-	<List>
-		{categories.map((category) => (
-			<Item key={category.id}>
-				<Category {...category} />
-			</Item>
-		))}
-	</List>
-)
+	useEffect(() => {
+		window.fetch('https://petgram-api-mb.vercel.app/categories')
+			.then((res) => res.json())
+			.then((data) => {
+				setCategories(data)
+			})
+	}, [])
+
+	return (
+		<List>
+			{categories.map((category) => (
+				<Item key={category.id}>
+					<Category {...category} />
+				</Item>
+			))}
+		</List>
+	)
+}
