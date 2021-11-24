919718fd328a00f6e4895d944d31f416f7bac2d3
Mauro Brandan <maurobrandan.mb@gmail.com>
2021-11-24 11:44:29 -0300

    Hacer like como usuario registrado

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { Article, ImgWrapper, Img, ImgSkeleton, ButtonSkeleton } from './styles'
-import { useLocalStorage } from '../../hooks/useLocalStorage'
 import { useNearScreen } from '../../hooks/useNearScreen'
 import { useLikePhoto } from '../../hooks/useLikePhoto'
 import { FavButton } from '../FavButton'
@@ -9,15 +8,12 @@ import { FavButton } from '../FavButton'
 const DEFAULT_IMAGE =
 	'https://res.cloudinary.com/midudev/image/upload/w_300/q_80/v1560262103/dogs.png'
 
-export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
-	const key = `like-${id}`
-	const [liked, setLiked] = useLocalStorage(key, false)
+export const PhotoCard = ({ id, liked, likes = 0, src = DEFAULT_IMAGE }) => {
 	const [show, card] = useNearScreen()
 	const [toggleLike] = useLikePhoto()
 
 	const handleFavClick = () => {
-		!liked && toggleLike({ variables: { input: { id: id } } })
-		setLiked(!liked)
+		toggleLike({ variables: { input: { id: id } } })
 	}
 
 	return (
--- src/index.js
+++ src/index.js
@@ -1,13 +1,48 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client'
+import {
+	ApolloClient,
+	ApolloProvider,
+	InMemoryCache,
+	ApolloLink,
+	HttpLink,
+	from,
+} from '@apollo/client'
+import { onError } from '@apollo/client/link/error'
 import { Provider } from './Context'
-
 import { App } from './App'
 
+const authMiddleware = new ApolloLink((operation, forward) => {
+	const token = window.sessionStorage.getItem('token')
+	const authorization = token ? `Bearer ${token}` : ''
+
+	if (token) {
+		operation.setContext({
+			headers: {
+				authorization,
+			},
+		})
+	}
+
+	return forward(operation)
+})
+
+const errorMiddleware = onError(({ networkError }) => {
+	if (networkError && networkError.result.code === 'invalid_token') {
+		window.sessionStorage.removeItem('token')
+		window.location.href = '/user'
+	}
+})
+
 const client = new ApolloClient({
-	uri: 'https://petgram-api-mb.vercel.app/graphql',
 	cache: new InMemoryCache(),
+	link: from([
+		authMiddleware,
+		new HttpLink({
+			uri: 'https://petgram-api-mb.vercel.app/graphql',
+		}),
+	]),
+	onError: errorMiddleware,
 })
 
 ReactDOM.render(
