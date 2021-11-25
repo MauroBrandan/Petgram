10f27073e2f160bb8d7abcacd89f2dae59dd9ae2
Mauro Brandan <maurobrandan.mb@gmail.com>
2021-11-25 11:11:06 -0300

    Redirect a login Pagina NotFound

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import { Detail } from './pages/Detail'
 import { Favorites } from './pages/Favorites'
 import { User } from './pages/User'
 import { NotRegisteredUser } from './pages/NotRegisteredUser'
+import { NotFound } from './pages/NotFound'
 
 export const App = () => {
 	const { isAuth } = useContext(Context)
@@ -21,8 +22,10 @@ export const App = () => {
 				<Route path='/' element={<Home />} />
 				<Route path='/pet/:categoryId' element={<Home />} />
 				<Route path='/detail/:detailId' element={<Detail />} />
-				<Route path='/favs' element={isAuth ? <Favorites /> : <NotRegisteredUser />} />
-				<Route path='/user' element={isAuth ? <User /> : <NotRegisteredUser />} />
+				<Route path='/favs' element={isAuth ? <Favorites /> : <Navigate to='/login' />}/>
+				<Route path='/user' element={isAuth ? <User /> : <Navigate to='/login' />} />
+				<Route path='/login' element={!isAuth ? <NotRegisteredUser /> : <Navigate to='/' />} />
+				<Route path='*' element={<NotFound />}/>
 			</Routes>
 			<NavBar />
 		</BrowserRouter>
---
+++ src/pages/NotFound.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const NotFound = () => {
+	return (
+		<div style={{ textAlign: 'center', width: '50%', margin: '50% auto' }}>
+			<h2>Lo siento, no encontramos lo que buscabas</h2>
+			<h3>:(</h3>
+		</div>
+	)
+}
